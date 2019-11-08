63c8419089d056ab330d3f964c0a91bcdb6f4890
Anantha Subramony Sreenivasan <guru059@gmail.com>
2019-11-08 08:52:33 +0530

    16

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('<h1 style="color:black;">Java Home App - version-6!!</h1> \n');
+  res.send('<h1 style="color:black;">Java Home App - version-16!!</h1> \n');
 });
 
 app.listen(PORT, HOST);
